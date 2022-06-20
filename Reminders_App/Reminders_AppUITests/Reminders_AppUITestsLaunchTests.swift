644736d1fac5742777aecc2565d918ac85eb7daa
Yitong Pei <71160237+1tongp@users.noreply.github.com>
2022-06-20 22:33:07 +1000

    Reminder App

---
+++ Reminders_App/Reminders_AppUITests/Reminders_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Reminders_AppUITestsLaunchTests.swift
+//  Reminders_AppUITests
+//
+//  Created by Yitong Pei on 20/6/2022.
+//
+
+import XCTest
+
+class Reminders_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
