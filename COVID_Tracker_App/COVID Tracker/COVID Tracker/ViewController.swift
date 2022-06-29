4b4760dc1d633443ff3426460274af7718098e72
Yitong Pei <71160237+1tongp@users.noreply.github.com>
2022-06-29 20:02:24 +1000

    connected the data api, but still have some errors

--- COVID_Tracker_App/COVID Tracker/COVID Tracker/ViewController.swift
+++ COVID_Tracker_App/COVID Tracker/COVID Tracker/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 // Data of Covid cases
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     /*
      - Call APIs
@@ -18,16 +18,50 @@ class ViewController: UIViewController {
      - Update UI
      */
     
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var dayData: [DayData] = []{
+        didSet{
+            DispatchQueue.main.async{
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
     private var scope: APICaller.DataScope = .national
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Covid Cases"
-        
-        creatFilterButton()
+        configureTable()
+        createFilterButton()
+        fetchData()
     }
     
-    private func creatFilterButton(){
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    private func configureTable(){
+        view.addSubview(tableView)
+        tableView.dataSource = self
+    }
+    private func fetchData(){
+        APICaller.shared.getCovidData(for: scope) {[weak self]result in
+            switch result{
+            case .success(let dayData):
+                self?.dayData = dayData
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    private func createFilterButton(){
         
         let buttonTitle: String = {
             switch scope{
@@ -47,10 +81,29 @@ class ViewController: UIViewController {
     
     @objc private func didTapFilter(){
         let vc = FilterViewController()
+        vc.completion = { [weak self] state in
+            self?.scope = .state(state)
+            self?.fetchData()
+            self?.createFilterButton()
+        }
         let navVC = UINavigationController(rootViewController: vc)
         present(navVC, animated: true)
     }
-
-
+    
+    //MARK: - Table
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let data = dayData[indexPath.row]
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = createText(with: data)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dayData.count
+    }
+    
+    private func createText(with data: DayData) -> String?{
+        let dateString = DateFormatter.prettyFormatter.string(from: data.date)
+        return "\(dateString): \(data.count)"
+    }
 }
 
