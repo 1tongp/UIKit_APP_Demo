7bb80db3bd7cca1eabbee7c647aceb4204361c54
Yitong Pei <71160237+1tongp@users.noreply.github.com>
2022-07-06 16:30:29 +1000

    init the IG Demo App

---
+++ Instagram_Demo_App/Instagram Demo App/Instagram Demo App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram Demo App
+//
+//  Created by Yitong Pei on 6/7/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
